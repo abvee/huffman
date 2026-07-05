f8a8532f018772e0956bef10c069b7db9b308eb8
abvee <icaliberdev@gmail.com>
2026-07-05 21:00:20 +0530

    After all, why shouldn't I make a giant array to use as a direct map Why not ? The computer has enough memory, and it makes my life easier....
    LIES ! This is the reason computing is so bad, a thousand tiny sacrifices by
    the machine, death by a million useless instructions.
    
    That being said, I will still be using a giant hash map to count unique
    characters, because I'm lazy

--- src/main.c
+++ src/main.c
@@ -2,26 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
-enum {MAX = 128};
+#include "common.h"
+#include "p-queue.c"
 
 struct {
 	signed char *ptr;
 	unsigned int capacity;
 	unsigned int len;
 } buf = {NULL, 0, 0}; // general purpose buffer
+/*
+giant fuck off hash map for every possible character
+*/
+unsigned int hash_map[HM_LEN];
 
 void read_input();
 
 int main(int argc, char *argv[]) {
 	buf.capacity = MAX;
+	memset(hash_map, 0, HM_LEN);
 
 	// handles all the buffer stuff
 	read_input();
+	// debug
+	printf("%s", buf.ptr);
+	printf("%d\n", buf.len);
+	printf("%d\n", buf.capacity);
 
 	/*
 	count of all the unique characters
 	*/
+	for (int i = 0; i < buf.len; i++)
+		hash_map[buf.ptr[i]]+=1;
+	// Now put in p_queue
+	for (int i = 0; i < HM_LEN; i++)
+		if (hash_map[i]) pq_enqueue((struct character) {i, hash_map[i]});
 	return 0;
 }
 
