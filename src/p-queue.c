240cb1691fb3035850bd7d9e5e6de13432214abf
abvee <icaliberdev@gmail.com>
2026-07-05 21:53:01 +0530

    I forgor :skull: to shift elements when inserting in the queue
    Also since HM_LEN is power of 2, we can safely use the fast ring buffer & operation with a mask
    
    Extra debug queue print function

--- src/p-queue.c
+++ src/p-queue.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include <stdio.h>
 
 /*
 I wanted to make this a dynamic array, but if we're hardcoding the hash map as
@@ -11,13 +12,17 @@ constexpr unsigned int MASK = HM_LEN - 1; // should be 2 ** 8 - 1
 
 bool pq_enqueue(struct character c) {
 	// overflow
-	if ((back + 1) % HM_LEN == front) return false;
+	if ((back + 1) & MASK == front) return false;
 	
 	// enqueue the actual character
 	int i = front;
 	for (; i < back && queue[i].count <= c.count; i = (i + 1) & MASK);
-	queue[i] = c;
 	back = (back + 1) & MASK;
+
+	for (int j = back; j != i; j = (j - 1) & MASK)
+		queue[j] = queue[(j - 1) & MASK];
+
+	queue[i] = c;
 	return true;
 }
 
@@ -28,4 +33,14 @@ struct character pq_dequeue() {
 	unsigned int f = front; // tmp
 	front = (front + 1) & MASK;
 	return queue[f];
+}
+
+void pq_print() {
+	for (int i = front; i < back; i++) {
+		if ((queue[i].c >= 'A' && queue[i].c <= 'Z') || (queue[i].c >= 'a' && queue[i].c <= 'z'))
+			printf("%c(%d):%d ", queue[i].c, queue[i].c, queue[i].count);
+		else
+			printf("(%d):%d ", queue[i].c, queue[i].count);
+	}
+	printf("\n");
 }
