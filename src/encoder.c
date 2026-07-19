0df2a188bc0c54b614a315fb1409ff520fe6e97e
abvee <icaliberdev@gmail.com>
2026-07-19 08:19:31 +0530

    I chose to instead move the handling of the current and prev pointers to encode() and write to the output buffer in encode()
    This avoids a peek() function in priority queue while leaking some of generating canon codes (setting the first one up) to the calling function.
    
    gen_canon_codes is now a tighter function that just generates each canonical
    code

--- src/encoder.c
+++ src/encoder.c
@@ -9,7 +9,10 @@ static inline void bit_lenghts(
 	unsigned long *op_len
 );
 static inline struct character *build_tree(struct character *tree);
-static inline void gen_canon_codes(byte op_buffer[]);
+static inline void gen_canon_codes(
+	struct character *current,
+	struct character *prev
+);
 static inline unsigned int count_unique_characters(
 	byte *buf,
 	unsigned int buf_len
@@ -57,10 +60,41 @@ void encode(byte *buf, unsigned int buf_len) {
 	);
 	op_buffer[0] = char_count - 1; // 0 -> 255
 
-	// write canon codes to encodings[]
-	// dequeues the priority queue, so this function also has to write the
-	// canon lengths to the output buffer
-	gen_canon_codes(op_buffer);
+	// dequeue each character, write pair lengths to op_buffer and generate canon codes
+	struct character *prev = pq_dequeue();
+	// 0 initial encoding
+	encodings[prev->c].n_bits = prev->count;
+	memset(encodings[prev->c].bits, 0, sizeof encodings[prev->c].bits);
+
+	// write to output buffer
+	unsigned int op_buf_i = 1;
+	op_buffer[op_buf_i] = prev->c;
+	op_buffer[op_buf_i + 1] = prev->count;
+	op_buf_i += 2;
+
+	// debug print
+	printf("%c(%d): %d ->\t", prev->c, prev->c, encodings[prev->c].n_bits);
+	for (int i = 0; i < sizeof encodings[prev->c].bits / sizeof encodings[prev->c].bits[0]; i++)
+		printf("0x%016lx ", encodings[prev->c].bits[i]);
+	printf("\n");
+
+	for (struct character *current; current = pq_dequeue(); prev = current) {
+		// write to output buffer
+		op_buffer[op_buf_i] = current->c;
+		op_buffer[op_buf_i + 1] = current->count;
+		op_buf_i += 2;
+
+		gen_canon_codes(current, prev);
+
+		// debug print
+		if (current->c > ' ' && current->c <= '~')
+			printf("%c(%d): %d ->\t", current->c, current->c, encodings[current->c].n_bits);
+		else
+			printf("(%d): %d ->\t", current->c, encodings[current->c].n_bits);
+		for (int i = 0; i < sizeof encodings[current->c].bits / sizeof encodings[current->c].bits[0]; i++)
+			printf("0x%016lx ", encodings[current->c].bits[i]);
+		printf("\n");
+	}
 
 	free(tree);
 	free(op_buffer);
@@ -177,87 +211,55 @@ static inline struct character *build_tree(struct character *tree) {
 }
 
 // generate canonical codes and fill in encodings[]
-static inline void gen_canon_codes(byte op_buffer[]) {
-	struct character *prev = pq_dequeue();
-	// write to output buffer
-	unsigned int op_buf_i = 1;
-	op_buffer[op_buf_i] = prev->c;
-	op_buffer[op_buf_i + 1] = prev->count;
-	op_buf_i += 2;
+static inline void gen_canon_codes(
+	struct character *current,
+	struct character *prev
+) {
+	unsigned int c_in = current->c;
+	encodings[c_in].n_bits = current->count;
 
-	encodings[prev->c].n_bits = prev->count;
+	// copy previous bits over
+	memcpy(
+		encodings[c_in].bits,
+		encodings[prev->c].bits,
+		sizeof encodings[c_in].bits
+	);
 
-	// debug print
-	printf("%c(%d): %d ->\t", prev->c, prev->c, encodings[prev->c].n_bits);
-	for (int i = 0; i < sizeof encodings[prev->c].bits / sizeof encodings[prev->c].bits[0]; i++) {
-		encodings[prev->c].bits[i] = 0;
-		printf("0x%016lx ", encodings[prev->c].bits[i]);
-	}
-	printf("\n");
+	// add 1
+	for (int i = 0; !++encodings[c_in].bits[i++];);
 
+	// shift bits
+	unsigned int shift = encodings[c_in].n_bits - encodings[prev->c].n_bits;
 
-	for (struct character *current; current = pq_dequeue();) {
-		// write to output buffer
-		op_buffer[op_buf_i] = current->c;
-		op_buffer[op_buf_i + 1] = current->count;
-		op_buf_i += 2;
+	/*
+	Okay, so basically if shift > 64, we have a problem. Shifting by > 64
+	bits means we lose the everything in that array cell.
 
-		encodings[current->c].n_bits = current->count;
-
-		// copy previous bits over
-		memcpy(
-			encodings[current->c].bits,
-			encodings[prev->c].bits,
-			sizeof encodings[current->c].bits
-		);
-
-		// add 1
-		for (int i = 0; !++encodings[current->c].bits[i++];);
-
-		// shift bits
-		unsigned int shift = encodings[current->c].n_bits - encodings[prev->c].n_bits;
-
-		/*
-		Okay, so basically if shift > 64, we have a problem. Shifting by > 64
-		bits means we lose the everything in that array cell.
-
-		So, we compute where we'll end up if we were to shift there, copy the
-		bytes over. Only then do we shift by (shift % TYPE_LEN_BITS)
-		*/
-		{
-			unsigned int current_byte = encodings[current->c].n_bits;
-			unsigned int index = (encodings[current->c].n_bits + shift) / TYPE_LEN_BITS;
-			for (int i = current_byte; i >= 0 && index > current_byte; index--, i--) {
-				encodings[current->c].bits[index] = encodings[current->c].bits[i];
-				encodings[current->c].bits[i] = 0;
-			}
+	So, we compute where we'll end up if we were to shift there, copy the
+	bytes over. Only then do we shift by (shift % TYPE_LEN_BITS)
+	*/
+	{
+		unsigned int current_byte = encodings[c_in].n_bits;
+		unsigned int index = (encodings[c_in].n_bits + shift) / TYPE_LEN_BITS;
+		for (int i = current_byte; i >= 0 && index > current_byte; index--, i--) {
+			encodings[c_in].bits[index] = encodings[c_in].bits[i];
+			encodings[c_in].bits[i] = 0;
 		}
-
-		/*
-		now we handle the remaining shift % 64 bits of shifting
-		Note that if shift % 64 is 0, then TYPE_LEN_BITS - shift is UB (can't
-		shift 64 bits) hence the if (shift)
-		*/
-		shift &= TYPE_LEN_BITS - 1;
-		if (shift) for (int i = 1; i < HM_LEN / TYPE_LEN_BITS; i++) {
-			encodings[current->c].bits[i] =
-				(encodings[current->c].bits[i] << shift)
-				|
-				(encodings[current->c].bits[i - 1] >> (TYPE_LEN_BITS - shift));
-		}
-		encodings[current->c].bits[0] <<= shift;
-
-		// debug print
-		if (current->c > ' ' && current->c <= '~')
-			printf("%c(%d): %d ->\t", current->c, current->c, encodings[current->c].n_bits);
-		else
-			printf("(%d): %d ->\t", current->c, encodings[current->c].n_bits);
-		for (int i = 0; i < sizeof encodings[current->c].bits / sizeof encodings[current->c].bits[0]; i++)
-			printf("0x%016lx ", encodings[current->c].bits[i]);
-		printf("\n");
-
-		prev = current;
 	}
+
+	/*
+	now we handle the remaining shift % 64 bits of shifting
+	Note that if shift % 64 is 0, then TYPE_LEN_BITS - shift is UB (can't
+	shift 64 bits) hence the if (shift)
+	*/
+	shift &= TYPE_LEN_BITS - 1;
+	if (shift) for (int i = 1; i < HM_LEN / TYPE_LEN_BITS; i++) {
+		encodings[c_in].bits[i] =
+			(encodings[c_in].bits[i] << shift)
+			|
+			(encodings[c_in].bits[i - 1] >> (TYPE_LEN_BITS - shift));
+	}
+	encodings[c_in].bits[0] <<= shift;
 }
 
 static inline void serialize(byte *buf, unsigned int buf_len, FILE *stream) {
