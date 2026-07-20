7d21dfde060ed9fa9f7f5b615c2cf4782521141e
abvee <icaliberdev@gmail.com>
2026-07-20 14:28:12 +0530

    I have good news and bad news
    The good news is I wrote the output mapping code and it compiles and runs
    without segfaulting
    
    The bad news is that it compiles and runs on the first try. Not only have I
    guaranteed to have messed something up, I wrote it whilst listening to the
    halo theme song, so I don't even understand what I've written
    
    I have to pray it works because I don't know how I'm going to debug this.

--- src/encoder.c
+++ src/encoder.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include "common.h"
 #include "p-queue.c"
 
@@ -101,7 +102,7 @@ void encode(byte *buf, unsigned int buf_len) {
 		printf("\n");
 	}
 
-	// serialize(buf, buf_len, op_buffer + op_buf_i, stdout);
+	serialize(buf, buf_len, op_buffer + op_buf_i, stdout);
 
 	free(tree);
 	free(op_buffer);
@@ -269,12 +270,66 @@ static inline void gen_canon_codes(
 	encodings[c_in].bits[0] <<= shift;
 }
 
+struct {
+	union {
+		uint64_t ac;
+		byte raw_bytes[sizeof(uint64_t)];
+	};
+	unsigned long i; // index
+} accumulator = {.ac = 0, .i = 0};
+
+enum {
+	AC_BIT_LEN = sizeof accumulator.ac * 8,
+};
+
+static inline unsigned long ac_shift(
+	unsigned int shift_amount,
+	byte encode_index,
+	unsigned int n_bits
+) {
+	assert(shift_amount < AC_BIT_LEN - 1);
+	/*
+	We should never be given a shift amount that's greater than the number of
+	bits left to encode for that character
+	*/
+	assert(shift_amount >= n_bits);
+
+	accumulator.ac <<= shift_amount;
+
+	unsigned int encode_byte_index = n_bits / TYPE_LEN_BITS;
+
+	// will never trigger if encode_byte_index is 0 thanks to above assert
+	if (n_bits & (TYPE_LEN_BITS - 1) < shift_amount) {
+		accumulator.ac |=
+			encodings[encode_index].bits[encode_byte_index] << (shift_amount - (n_bits & (TYPE_LEN_BITS - 1)));
+		encode_index -= 1;
+
+		shift_amount -= n_bits & (TYPE_LEN_BITS - 1);
+	}
+
+	accumulator.ac |=
+		encodings[encode_index].bits[encode_byte_index] >> (TYPE_LEN_BITS - shift_amount);
+	return accumulator.i + shift_amount;
+}
+
+static inline void ac_flush(byte *op_buffer) {
+	// go backwards for little endian
+	for (
+		int i = sizeof accumulator.raw_bytes / sizeof *accumulator.raw_bytes - 1;
+		i >= 0;
+		i--
+	) *op_buffer++ = accumulator.raw_bytes[i];
+	accumulator.i = 0;
+}
+
 static inline void serialize(
 	byte *in_buffer,
 	unsigned int in_buffer_len,
 	byte *op_buffer, // assume the op buffer has enough space
 	FILE *stream
 ) {
+	accumulator.ac = 0;
+
 	printf("\n--Serialization--\n");
 	for (unsigned int i = 0; i < in_buffer_len; i++) {
 		// debug print
@@ -285,5 +340,23 @@ static inline void serialize(
 		for (int j = 0; j < sizeof (*encodings).bits / sizeof *(*encodings).bits; j++)
 			printf("0x%016lx ", encodings[in_buffer[i]].bits[j]);
 		printf("\n");
+
+		unsigned int n_bits = encodings[in_buffer[i]].n_bits;
+		while (n_bits >= AC_BIT_LEN - (accumulator.i + 1)) {
+
+			// shift by some number of bits
+			ac_shift(AC_BIT_LEN - (accumulator.i + 1), in_buffer[i], n_bits);
+			n_bits -= AC_BIT_LEN - (accumulator.i + 1);
+
+			ac_flush(op_buffer);
+			op_buffer += sizeof accumulator.ac;
+		}
+
+		// shift remaining bits
+		ac_shift(n_bits, in_buffer[i], n_bits);
+
+		if (accumulator.i >= AC_BIT_LEN)
+			ac_flush(op_buffer);
 	}
+	// TODO: flush whatever remains in the accumulator
 }
