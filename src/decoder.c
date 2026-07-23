9992dfcadfa9a48b320ec605e7deab09f5d3c891
abvee <icaliberdev@gmail.com>
2026-07-23 18:04:02 +0530

    Generate decoder cannon codes and debug print

--- src/decoder.c
+++ src/decoder.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include "common.h"
 #include "p-queue.c"
+#include "canon-codes.c"
 
 static inline uint read_characters(
 	struct character *characters,
@@ -16,6 +17,32 @@ void decode(byte *buf, uint buf_len) {
 	struct character *characters = malloc(sizeof *characters * char_count);
 	uint max_bit_len = read_characters(characters, buf + buf_i, char_count);
 	buf_i += 2 * char_count;
+
+	// create lookup table
+
+
+	/*
+	Generate canon codes and write them to your mother
+	*/
+	struct character *prev = pq_dequeue();
+	// 0 initial encoding
+	encodings[prev->c].n_bits = prev->count;
+	memset(encodings[prev->c].bits, 0, sizeof (*encodings).bits);
+
+	for (struct character *current; current = pq_dequeue(); prev = current) {
+		gen_canon_codes(current, prev);
+
+		// debug print
+		if (isalnum(current->c))
+			f_printf("%c(%d): %d ->\t", current->c, current->c, encodings[current->c].n_bits);
+		else
+			f_printf("(%d): %d ->\t", current->c, encodings[current->c].n_bits);
+		for (int i = 0; i < sizeof (*encodings).bits / sizeof *(*encodings).bits; i++)
+			f_printf("0x%016lx ", encodings[current->c].bits[i]);
+		f_printf("\n");
+
+		// write_table(current->c);
+	}
 
 	free(characters);
 }
